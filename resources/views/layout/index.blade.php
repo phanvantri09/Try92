3519889d4887992c7cc785f701ab758f660e8885
phongnv9 <phongnv9@rikkeisoft.com>
2023-10-24 16:52:59 +0700

    logo

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Try92</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('/img/imageTry92/logo.jpg') }}">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
